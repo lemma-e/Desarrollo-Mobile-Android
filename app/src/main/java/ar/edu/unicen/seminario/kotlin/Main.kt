e1a6d54289f6918239225a2a6b170d294f6c8efd
IgnacioLemma <ilemma@alumnos.exa.unicen.edu.ar>
2026-09-05 21:09:02 -0300

    conceptos basicos

---
+++ app/src/main/java/ar/edu/unicen/seminario/kotlin/Main.kt
@@ -0,0 +1,55 @@
+package ar.edu.unicen.seminario.kotlin
+
+const val PI = 3.1416
+fun main(){
+
+    println("Hello world!")
+
+    //val no cambia valor
+    // kt entiende por si solo el tipo de variable
+    val nombre = "Ede"
+    println("hello $nombre")
+
+    //var si puede cambiar de valor
+    //? -> permite null
+    var nombre2: String? = "test"
+    nombre2 = "si"
+    println(nombre2)
+    nombre2 = null;
+    println(nombre2)
+
+    //any(no tengo tipo), unit(void), nothing(nunca retorna un resultado)
+    var test: Any = true
+    test = "true"
+    test = 1
+    println(test)
+
+    println("///////////")
+
+    val l1 = ArrayList<Int>()
+    l1.add(1)
+    l1.add(2)
+    l1.add(4)
+    println(l1)
+
+    val l2 = listOf(1,2,3,4,5)
+    println(l2)
+
+    val l3 = mutableListOf(6,7,8)
+    l3.add(9)
+    println(l3)
+
+    val l4 = setOf(9,10,11)
+    println(l4)
+
+    val l5 = mutableSetOf(12)
+    l5.add(13)
+    println(l5)
+
+    val l6 = mapOf(1 to "uno")
+    println(l6)
+
+    val l7 = mutableMapOf(2 to "dosh")
+    l7[1] = "tres"
+    println(l7)
+}
